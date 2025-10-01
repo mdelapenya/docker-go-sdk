461793d9f90f5b26e945d8398ce6da07b6e1c366
Manuel de la Peña <mdelapenya@gmail.com>
2025-10-01 14:36:59 +0200

    fix: set proper versions (#99)

--- network/version.go
+++ network/version.go
@@ -3,7 +3,7 @@ package network
 import "github.com/docker/go-sdk/client"
 
 const (
-	version     = "0.1.0-alpha009"
+	version     = "0.1.0-alpha010"
 	moduleLabel = client.LabelBase + ".network"
 )
 
--- volume/version.go
+++ volume/version.go
@@ -3,7 +3,7 @@ package volume
 import "github.com/docker/go-sdk/client"
 
 const (
-	version     = "0.1.0-alpha009"
+	version     = "0.1.0-alpha002"
 	moduleLabel = client.LabelBase + ".volume"
 )
 
